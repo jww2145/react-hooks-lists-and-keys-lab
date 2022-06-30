90d34098024d0442b3ef2cfa566f23a2c3f2c7ec
Joshua Wu <jww2145@columbia.edu>
2022-06-30 14:54:38 -0400

    commit tree 16446f03f4afd37e6cbec002ed9aa216db8fbc17

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const createSpan = technologies.map((technology) =>{
+    return(
+      <span>{technology}</span>
+    )
+  }
+  )
+
+
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {createSpan}
       </div>
     </div>
   );
